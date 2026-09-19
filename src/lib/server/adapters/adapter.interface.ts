59c8e32d3c59ecea67bafa249afaf95c89397c7a
Jérémy Munsch <github@jeremydev.ovh>
2026-09-19 12:01:48 +0200

    refactor: step2

---
+++ src/lib/server/adapters/adapter.interface.ts
@@ -0,0 +1,5 @@
+/**
+ * Contrat d'interface pour les adaptateurs de monitoring.
+ * Ré-exporte l'interface canonique depuis le modèle de types centralisé.
+ */
+export type { MonitoringAdapter, AdapterConfig } from '$lib/types';
